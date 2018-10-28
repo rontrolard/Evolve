b9d90b2274d3b4f8cf55b11480cfa0bd9fa16e87
Philippe Lécaillon <lecaillon@hotmail.com>
2018-10-28 11:29:13 +0100

    Check postgresql version before using PROCEDURE

--- test/Evolve.IntegrationTest/PostgreSQL/Resources/Sql_Scripts/Migration/V6_1__procedures.sql
+++ test/Evolve.IntegrationTest/PostgreSQL/Resources/Sql_Scripts/Migration/V6_1__procedures.sql
@@ -1,8 +1,16 @@
-CREATE PROCEDURE insert_data(a integer, b integer)
-LANGUAGE SQL
-AS $$
-INSERT INTO distributors3 VALUES (a);
-INSERT INTO distributors3 VALUES (b);
-$$;
+DO $$
+ DECLARE ver integer;
+ BEGIN
+  SELECT current_setting('server_version_num') INTO ver;
+  IF (ver >= 110000) THEN
+	CREATE PROCEDURE insert_data(a integer, b integer)
+	LANGUAGE SQL
+	AS $$
+	INSERT INTO distributors3 VALUES (a);
+	INSERT INTO distributors3 VALUES (b);
+	$$;
 
-CALL insert_data(1, 2);
+	CALL insert_data(1, 2);
+  END IF;
+ END
+$$;
